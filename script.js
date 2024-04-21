9187097d4f5e7995e6b8c72016d962cfec5dc5f3
Fortune O. Iyoha <dev.fortune.io@gmail.com>
2024-04-22 00:09:12 +0100

    Update Files
    Fixed delete task issue.

--- script.js
+++ script.js
@@ -1,60 +1,37 @@
 const taskInput = document.getElementById("task-input");
-const submitBtn = document.querySelector(".btn-submit");
+const submitBtn = document.getElementById("btn-submit");
 const infoText = document.querySelector(".info-text");
 const todoList = document.querySelector(".todo-list");
-const todoItems = document.querySelectorAll(".todo-list .todo-item");
-const deleteBtn = document.querySelector(".btn-delete");
 
-// Add task when submit button is clicked
-submitBtn.addEventListener("click", (e) => {
+submitBtn.addEventListener("click", createTask);
+todoList.addEventListener("click", deleteTask);
+
+function createTask(e) {
   e.preventDefault();
-  createTask();
-});
 
-// Delete task when delete button is clicked
-// TODO: Add functionality to delete a task after creation
-// deleteBtn.addEventListener("click", deleteTask());
-
-function createTask() {
-  // Stores the value of inputted text
-  const taskText = taskInput.value;
-
-  // Create the delete button
   const deleteBtn = document.createElement("button");
+  const todoItem = document.createElement("li");
 
-  // Creates the list item
-  const li = document.createElement("li");
+  todoItem.className = "todo-item";
+  todoItem.textContent = taskInput.value;
 
-  // Adds a type attribute set to `button`.
-  // Considered best practices
   deleteBtn.setAttribute("type", "button");
-
-  // Adds DOM token classlist
-  li.className = "todo-item";
   deleteBtn.className = "btn btn-delete";
-
-  // Sets the text of list item to input value
-  li.textContent = taskText;
-
-  // Add delete button text
   deleteBtn.textContent = "Delete";
 
-  // Checks if input contains text
-  if (taskText) {
-    // Create a task if it does
-    todoList.appendChild(li);
-
-    // Add delete button to each task
-    li.appendChild(deleteBtn);
-
-    // Display no error message
+  if (taskInput.value) {
+    todoList.appendChild(todoItem);
+    todoItem.appendChild(deleteBtn);
     infoText.textContent = "";
+    // FIXME:
+    // localStorage.setItem(null, taskInput.value);
   } else {
-    // Display an error message
-    infoText.textContent = "Please input a task";
+    infoText.textContent = "Please add input for a task";
   }
 }
 
-function deleteTask() {
-  console.log("deleted");
+function deleteTask(e) {
+  if (e.target.classList.contains("btn-delete") && confirm("Are you sure?")) {
+    e.target.parentElement.remove();
+  }
 }
